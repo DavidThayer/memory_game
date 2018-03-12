d14f6e4ff69f3a0aa7ed10e4aadef49758df4c3c
David Thayer <david.thayer@gmail.com>
2018-03-11 22:40:19 -0700

    logic almost complete

--- js/main.js
+++ js/main.js
@@ -23,15 +23,27 @@ var cards = [
 ];
 var cardsInPlay = [];
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+
+  this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 }
-
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i );
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+};
 var checkForMatch = function (){
+    this.setAttribute('src',cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
       if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
@@ -41,5 +53,4 @@ var checkForMatch = function (){
     }
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
