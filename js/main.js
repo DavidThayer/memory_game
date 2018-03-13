5a55714a571189c60cee95c8201823d851f4a514
David Thayer <david.thayer@gmail.com>
2018-03-12 22:02:00 -0700

    logic running

--- js/main.js
+++ js/main.js
@@ -23,6 +23,16 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+var checkForMatch = function (){
+    if (cardsInPlay.length === 2) {
+      if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+      } else {
+        alert("Sorry, try again.");
+      }
+    }
+}
+
 var flipCard = function () {
   var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
@@ -33,24 +43,15 @@ var flipCard = function () {
   this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 }
+
 var createBoard = function(){
   for (var i = 0; i < cards.length; i++) {
     var cardElement = document.createElement('img');
     cardElement.setAttribute("src", "images/back.png");
-    cardElement.setAttribute("data-id", i );
-    cardElement.addEventListener('click', flipCard);
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
     document.getElementById("game-board").appendChild(cardElement);
   }
-};
-var checkForMatch = function (){
-    this.setAttribute('src',cards[cardId].cardImage);
-    if (cardsInPlay.length === 2) {
-      if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-      } else {
-        console.log("Sorry, try again.");
-      }
-    }
 }
 
 createBoard();
